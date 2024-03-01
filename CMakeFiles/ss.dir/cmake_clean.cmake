637a3a9587dd8f5d070225f9814051bc3be38560
PRAJWAL PURUSHOTHAM VAREKAR <rumor.vocal-0a@icloud.com>
2024-03-01 13:53:31 +0530

    Created Random Tree

---
+++ CMakeFiles/ss.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ss.dir/random_tree.cpp.o"
+  "CMakeFiles/ss.dir/random_tree.cpp.o.d"
+  "ss"
+  "ss.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ss.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
